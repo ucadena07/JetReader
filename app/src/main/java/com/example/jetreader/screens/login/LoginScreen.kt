29ea0ed0b80284ca08b226d367f0c2a517a9bb78
Ulises Cadena <ucadena07@gmail.com>
2023-12-24 09:26:22 -0600

    setting up email input 2

--- app/src/main/java/com/example/jetreader/screens/login/LoginScreen.kt
+++ app/src/main/java/com/example/jetreader/screens/login/LoginScreen.kt
@@ -4,13 +4,20 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -19,8 +26,13 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.jetreader.components.EmailInput
 import com.example.jetreader.components.ReaderLogo
 
 @Composable
@@ -28,6 +40,7 @@ fun LoginScreen(navController: NavHostController) {
     Surface(modifier = Modifier.fillMaxSize()) {
         Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Top) {
             ReaderLogo()
+            LoginForm()
         }
     }
 }
@@ -51,11 +64,15 @@ fun LoginForm(){
     }
     val modifier = Modifier
         .height(250.dp)
+        .fillMaxWidth()
         .background(MaterialTheme.colorScheme.background)
         .verticalScroll(
             rememberScrollState()
         )
     Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-
+        EmailInput(emailState = email, onAction = KeyboardActions {
+            passwordFocusRequest.requestFocus()
+        })
     }
 }
+
