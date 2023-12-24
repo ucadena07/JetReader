27e27e9e80a6c98d2334f4c1073e8c4f662b336e
Ulises Cadena <ucadena07@gmail.com>
2023-12-24 08:28:04 -0600

    animating the splash screen

--- app/src/main/java/com/example/jetreader/screens/ReaderSplashScreen.kt
+++ app/src/main/java/com/example/jetreader/screens/ReaderSplashScreen.kt
@@ -1,5 +1,8 @@
 package com.example.jetreader.screens
 
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -12,21 +15,34 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavHostController) {
+    val scale = remember {
+        Animatable(0f)
+    }
+    LaunchedEffect(key1 = true){
+        scale.animateTo(0.9f, tween(durationMillis = 800, easing = {OvershootInterpolator(8f).getInterpolation(it)}))
+        delay(2000L)
+    }
     Surface(modifier = Modifier
         .padding(15.dp)
-        .size(330.dp),
+        .size(330.dp).scale(scale.value),
         shape = CircleShape,
         color = Color.White,
         border = BorderStroke(width = 2.dp, color = Color.LightGray)
     ) {
+
+
         Column(modifier = Modifier
             .padding(15.dp),verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally){
             Text(text = "A. Reader", style= MaterialTheme.typography.headlineLarge, color = Color.Red.copy(0.5f))
