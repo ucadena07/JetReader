9d5f0374682a11c719c9da01358d658174c39460
Ulises Cadena <ucadena07@gmail.com>
2024-01-01 09:52:28 -0600

    adding the logout icon

--- app/src/main/java/com/example/jetreader/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/jetreader/screens/home/HomeScreen.kt
@@ -3,15 +3,19 @@ package com.example.jetreader.screens.home
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Logout
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -33,6 +37,8 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import com.example.jetreader.R
+import com.example.jetreader.navigation.ReaderScreens
+import com.google.firebase.auth.FirebaseAuth
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -83,9 +89,16 @@ fun ReaderAppBar(
                         color = Color.Red.copy(0.9f),
                         style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
                     )
+                    Spacer(modifier = Modifier.width(150.dp))
                 }
         },
-        actions = {},
+        actions = {
+                  IconButton(onClick = { FirebaseAuth.getInstance().signOut().run {
+                      navController.navigate(ReaderScreens.LoginScreen.name)
+                  } }) {
+                      Icon(imageVector = Icons.Default.Logout, contentDescription = "logout", tint = Color(0xFF92CBDF))
+                  }
+        },
         colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.Transparent)
     )
 }
