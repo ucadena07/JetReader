0a2d5bbe7b9878aa3c24892b302f5e8327d7de77
Ulises Cadena <ucadena07@gmail.com>
2024-01-01 09:44:24 -0600

    creating the top bar

--- app/src/main/java/com/example/jetreader/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/jetreader/screens/home/HomeScreen.kt
@@ -1,10 +1,91 @@
 package com.example.jetreader.screens.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import com.example.jetreader.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(navController: NavHostController) {
-    Text(text = "home screen")
+   Scaffold(topBar = {
+                     ReaderAppBar(title = "A.Reader", showProfile = true, navController = navController)
+   }, floatingActionButton = {
+        FABContent{
+            
+        }
+   }) {
+    Surface(modifier = Modifier
+        .padding(it)
+        .fillMaxSize()) {
+
+    }
+   }
+}
+
+@Composable
+fun FABContent(onTap: () -> Unit) {
+    FloatingActionButton(onClick = { onTap()},
+        shape = RoundedCornerShape(50.dp),
+        containerColor = Color(0xFF92CBDF)) {
+        Icon(imageVector = Icons.Default.Add, contentDescription = "Add a Book", tint = MaterialTheme.colorScheme.onSecondary)
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ReaderAppBar(
+    title: String,
+    showProfile: Boolean,
+    navController: NavController
+){
+    TopAppBar(
+        title = { 
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    if(showProfile){
+                        Icon(imageVector = Icons.Default.Favorite, contentDescription = "icon", modifier = Modifier
+                            .clip(
+                                RoundedCornerShape(12.dp)
+                            )
+                            .scale(0.6f))
+                    }
+                    Text(
+                        text = title,
+                        color = Color.Red.copy(0.9f),
+                        style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
+                    )
+                }
+        },
+        actions = {},
+        colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.Transparent)
+    )
 }
