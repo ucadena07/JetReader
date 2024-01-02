d96c8bc58591c1a68cfddc1003fd422d33cc7160
Ulises Cadena <ucadena07@gmail.com>
2024-01-02 06:39:31 -0600

    book listcard

--- app/src/main/java/com/example/jetreader/components/Components.kt
+++ app/src/main/java/com/example/jetreader/components/Components.kt
@@ -1,27 +1,49 @@
 package com.example.jetreader.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Logout
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.jetreader.navigation.ReaderScreens
+import com.google.firebase.auth.FirebaseAuth
 
 @Composable
 fun ReaderLogo(modifier: Modifier = Modifier) {
@@ -112,5 +134,61 @@ fun PasswordVisibility(passwordVisibility: MutableState<Boolean>) {
     val visible = passwordVisibility.value
     IconButton(onClick = { passwordVisibility.value = !visible}) {
         Icons.Default.Close
+    }
+}
+
+@Composable
+fun TitleSection(modifier: Modifier = Modifier, label:String){
+    Surface(modifier = modifier.padding(start = 5.dp,top = 1.dp)) {
+        Column {
+            Text(text = label,
+                fontSize = 19.sp,
+                fontStyle = FontStyle.Normal,
+                textAlign = TextAlign.Left)
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ReaderAppBar(
+    title: String,
+    showProfile: Boolean,
+    navController: NavController
+){
+    TopAppBar(
+        title = {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                if(showProfile){
+                    Icon(imageVector = Icons.Default.Favorite, contentDescription = "icon", modifier = Modifier
+                        .clip(
+                            RoundedCornerShape(12.dp)
+                        )
+                        .scale(0.6f))
+                }
+                Text(
+                    text = title,
+                    color = Color.Red.copy(0.9f),
+                    style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
+                )
+                Spacer(modifier = Modifier.width(150.dp))
+            }
+        },
+        actions = {
+            IconButton(onClick = { FirebaseAuth.getInstance().signOut().run {
+                navController.navigate(ReaderScreens.LoginScreen.name)
+            } }) {
+                Icon(imageVector = Icons.Default.Logout, contentDescription = "logout", tint = Color(0xFF92CBDF))
+            }
+        },
+        colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.Transparent)
+    )
+}
+@Composable
+fun FABContent(onTap: () -> Unit) {
+    FloatingActionButton(onClick = { onTap()},
+        shape = RoundedCornerShape(50.dp),
+        containerColor = Color(0xFF92CBDF)) {
+        Icon(imageVector = Icons.Default.Add, contentDescription = "Add a Book", tint = MaterialTheme.colorScheme.onSecondary)
     }
 }
--- app/src/main/java/com/example/jetreader/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/jetreader/screens/home/HomeScreen.kt
@@ -1,52 +1,44 @@
 package com.example.jetreader.screens.home
-
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Logout
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
-import com.example.jetreader.R
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.jetreader.components.FABContent
+import com.example.jetreader.components.ReaderAppBar
+import com.example.jetreader.components.TitleSection
 import com.example.jetreader.model.MBook
 import com.example.jetreader.navigation.ReaderScreens
 import com.google.firebase.auth.FirebaseAuth
@@ -69,67 +61,16 @@ fun HomeScreen(navController: NavHostController) {
    }
 }
 
-@Composable
-fun FABContent(onTap: () -> Unit) {
-    FloatingActionButton(onClick = { onTap()},
-        shape = RoundedCornerShape(50.dp),
-        containerColor = Color(0xFF92CBDF)) {
-        Icon(imageVector = Icons.Default.Add, contentDescription = "Add a Book", tint = MaterialTheme.colorScheme.onSecondary)
-    }
-}
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun ReaderAppBar(
-    title: String,
-    showProfile: Boolean,
-    navController: NavController
-){
-    TopAppBar(
-        title = { 
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    if(showProfile){
-                        Icon(imageVector = Icons.Default.Favorite, contentDescription = "icon", modifier = Modifier
-                            .clip(
-                                RoundedCornerShape(12.dp)
-                            )
-                            .scale(0.6f))
-                    }
-                    Text(
-                        text = title,
-                        color = Color.Red.copy(0.9f),
-                        style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
-                    )
-                    Spacer(modifier = Modifier.width(150.dp))
-                }
-        },
-        actions = {
-                  IconButton(onClick = { FirebaseAuth.getInstance().signOut().run {
-                      navController.navigate(ReaderScreens.LoginScreen.name)
-                  } }) {
-                      Icon(imageVector = Icons.Default.Logout, contentDescription = "logout", tint = Color(0xFF92CBDF))
-                  }
-        },
-        colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.Transparent)
-    )
-}
+
+
 
 @Composable
 fun ReadingRightNowArea(books: List<MBook>, navController: NavController){
 
 }
 
-@Composable
-fun TitleSection(modifier: Modifier = Modifier, label:String){
-    Surface(modifier = modifier.padding(start = 5.dp,top = 1.dp)) {
-        Column {
-                Text(text = label,
-                    fontSize = 19.sp,
-                    fontStyle = FontStyle.Normal,
-                    textAlign = TextAlign.Left)
-        }
-    }
-}
+
 
 @Composable
 fun HomeContent(navController: NavController){
@@ -138,7 +79,7 @@ fun HomeContent(navController: NavController){
     }else{
         "N/A"
     }
-    Column(modifier = Modifier.padding(2.dp), verticalArrangement = Arrangement.SpaceEvenly) {
+    Column(modifier = Modifier.padding(2.dp), verticalArrangement = Arrangement.Top) {
         Row(modifier = Modifier.align(alignment = Alignment.Start)) {
             TitleSection(label = "Your reading \n " + " activity right now")
             Spacer(modifier = Modifier.fillMaxWidth(0.7f))
@@ -154,3 +95,48 @@ fun HomeContent(navController: NavController){
         }
     }
 }
+
+@Preview
+@Composable
+fun ListCard(book: MBook = MBook(id = "Afdf",title = "Running", authors = "rico cadena","hello world"),
+             onPressDetails: (String) -> Unit  = {}){
+
+    val context = LocalContext.current
+    val resources = context.resources
+    val displayMetrics = resources.displayMetrics
+    val screenWidth = displayMetrics.widthPixels / displayMetrics.density
+    val spacing = 10
+    Card(shape = RoundedCornerShape(29.dp),
+        elevation = CardDefaults.cardElevation(6.dp),
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .clickable {
+                onPressDetails.invoke(book.title!!)
+            },
+        colors = CardDefaults.cardColors(containerColor = Color.White)
+        ) {
+        Column(modifier = Modifier.width(screenWidth.dp - (spacing * 2).dp),
+            horizontalAlignment = Alignment.Start) {
+            Row(horizontalArrangement = Arrangement.Center) {
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data("")
+                        .crossfade(true)
+                        .build(),
+                    contentDescription = "book image",
+                    modifier = Modifier
+                        .height(140.dp)
+                        .width(100.dp)
+                        .padding(4.dp)
+                )
+                Spacer(modifier = Modifier.width(50.dp))
+                Column() {
+
+                }
+            }
+        }
+
+    }
+}
+
