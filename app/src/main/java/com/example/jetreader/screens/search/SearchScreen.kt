b4d23258e43aabb241b4bd0c56ba30de997a9d9d
Ulises Cadena <ucadena07@gmail.com>
2024-01-06 09:49:39 -0600

    search screen ui setup

--- app/src/main/java/com/example/jetreader/screens/search/SearchScreen.kt
+++ app/src/main/java/com/example/jetreader/screens/search/SearchScreen.kt
@@ -2,14 +2,28 @@ package com.example.jetreader.screens.search
 
 import android.renderscript.ScriptGroup.Input
 import android.util.Log
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -17,16 +31,26 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.example.jetreader.components.FABContent
 import com.example.jetreader.components.InputField
 import com.example.jetreader.components.ReaderAppBar
+import com.example.jetreader.model.MBook
 import com.example.jetreader.navigation.ReaderScreens
 import com.example.jetreader.screens.home.HomeContent
+import okhttp3.internal.wait
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -43,12 +67,71 @@ fun SearchScreen(navController: NavHostController) {
         Surface(modifier = Modifier
             .padding(it)
             .fillMaxSize()) {
-            SearchForm(modifier = Modifier.fillMaxWidth().padding(16.dp)){ search ->
-                Log.d("FB",search)
+            Column {
+                SearchForm(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)){ search ->
+                    Log.d("FB",search)
+                }
+                Spacer(modifier = Modifier.height(13.dp))
+                BookList(navController)
             }
 
+
+
         }
+
     }
+}
+
+@Composable
+fun BookList(navController: NavController) {
+    var list = listOf(
+        MBook(title = "dfdfdf"),
+        MBook(title = "xxxx"),
+        MBook(title = "yyyy"),
+        MBook(title = "dfduuuufdf"),
+    )
+   LazyColumn(modifier = Modifier.fillMaxSize(), contentPadding = PaddingValues(16.dp)){
+       items(items = list){book ->
+           BookRow(book,navController)
+       }
+   }
+}
+
+@Composable
+fun BookRow(book: MBook, navController: NavController) {
+   Card(modifier = Modifier
+       .clickable {
+
+       }
+       .fillMaxWidth()
+       .height(100.dp)
+       .padding(3.dp),
+       shape = RectangleShape,
+       colors = CardDefaults.cardColors(containerColor = Color.White),
+       elevation = CardDefaults.cardElevation(defaultElevation = 10.dp)) {
+       Row(modifier = Modifier.padding(5.dp), verticalAlignment = Alignment.Top) {
+           val imageUrl = "http://books.google.com/books/content?id=7cBZEAAAQBAJ&printsec=frontcover&img=1"
+           AsyncImage(
+               model = ImageRequest.Builder(LocalContext.current)
+                   .data(imageUrl)
+                   .crossfade(true)
+                   .build(),
+               contentDescription = "book image",
+               modifier = Modifier
+                   .fillMaxHeight()
+                   .width(80.dp)
+                   .padding(end = 4.dp)
+           )
+            Column {
+                Text(text = book.title.toString(), overflow = TextOverflow.Ellipsis)
+                Text(text = "Authors: ${book.authors}", overflow = TextOverflow.Clip, style = MaterialTheme.typography.titleSmall)
+
+
+            }
+       }
+   }
 }
 
 @OptIn(ExperimentalComposeUiApi::class)
