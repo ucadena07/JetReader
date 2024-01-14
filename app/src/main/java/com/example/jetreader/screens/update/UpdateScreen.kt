4300bcbeb873338a9deb7483e6632a659e708bf2
Ulises Cadena <ucadena07@gmail.com>
2024-01-14 09:42:21 -0600

    update screen

--- app/src/main/java/com/example/jetreader/screens/update/UpdateScreen.kt
+++ app/src/main/java/com/example/jetreader/screens/update/UpdateScreen.kt
@@ -1,14 +1,25 @@
 package com.example.jetreader.screens.update
 
 import android.util.Log
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -16,9 +27,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.example.jetreader.components.FABContent
 import com.example.jetreader.components.ReaderAppBar
 import com.example.jetreader.data.DataOrException
@@ -53,13 +70,90 @@ fun UpdateScreen(navController: NavHostController, id: String?,viewModel: HomeSc
                     bookInfo.loading = false;
                 }else{
 
-                        Text(text = viewModel.data.value.data?.get(0)?.title!!)
-
+                    Surface(modifier = Modifier
+                        .padding(2.dp)
+                        .fillMaxWidth(), shape = CircleShape, shadowElevation = 4.dp) {
+                        ShowBookUpdate(bookInfo = viewModel.data.value, id)
+                    }
                 }
 
             }
 
 
         }
+    }
+}
+
+@Composable
+fun ShowBookUpdate(bookInfo: DataOrException<List<MBook>, Boolean, Exception>, bookId: String?) {
+    Log.d("ShowBookUpdate", bookId.toString())
+    Log.d("ShowBookUpdate", bookInfo.data.toString())
+    Row {
+        Spacer(modifier = Modifier.width(43.dp))
+        Column (modifier = Modifier.padding(4.dp), verticalArrangement = Arrangement.Center){
+            CardListItem(book = bookInfo.data!!.first(){
+                it -> it.id .toString() == bookId.toString()
+            }, onPressDetails = {})
+        }
+    }
+}
+
+@Composable
+fun CardListItem(book: MBook, onPressDetails: () -> Unit) {
+    Card(modifier = Modifier
+        .padding(
+            start = 4.dp,
+            end = 4.dp,
+            top = 4.dp,
+            bottom = 8.dp
+        )
+        .clip(RoundedCornerShape(20.dp))
+        .clickable { }, elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)) {
+        Row(horizontalArrangement = Arrangement.Start) {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(book.photoUrl)
+                    .crossfade(true)
+                    .build(),
+                contentDescription = "book image",
+                modifier = Modifier
+                    .height(100.dp)
+                    .width(120.dp)
+                    .padding(end = 4.dp)
+                    .clip(
+                        RoundedCornerShape(
+                            topStart = 120.dp, topEnd = 20.dp, bottomEnd = 0.dp, bottomStart = 0.dp
+                        )
+                    )
+            )
+
+            Column {
+                Text(text = book.title.toString(),
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier
+                        .padding(start = 8.dp, end = 8.dp)
+                        .width(120.dp),
+                    fontWeight = FontWeight.SemiBold,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis)
+
+                Text(text = book.authors.toString(),
+                    style = MaterialTheme.typography.titleMedium,
+                    modifier = Modifier.padding(start = 8.dp,
+                        end = 8.dp,
+                        top = 2.dp,
+                        bottom = 0.dp))
+
+                Text(text = book.publishedDate.toString(),
+                    style = MaterialTheme.typography.titleMedium,
+                    modifier = Modifier.padding(start = 8.dp,
+                        end = 8.dp,
+                        top = 0.dp,
+                        bottom = 8.dp))
+
+            }
+        }
+
+
     }
 }
